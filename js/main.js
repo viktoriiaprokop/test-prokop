95506ec591c3c72ab88db67e2fe203c5760fbb08
viktoriiaprokop <viktoriia.prokop@gmail.com>
2018-07-02 11:58:55 +0300

    styles, fonts and smooth scroll

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ xhr.onreadystatechange = function() {
   for (var i = 0; i < response.length; i++) {
     console.log(response[i]);
       var articleData = response[i];
-      htmlBlock += '<div class="first article">'
+      htmlBlock += '<div class="article">'
       +'<div>'
         +'<picture>'
           +'<img src="img/'+articleData.img+'">'
         +'</picture>'
       +'</div>'
-      +'<div>'
+      +'<div class="article-details">'
         +'<div class="date">'+articleData.date+'</div>'
           +'<h2>'+articleData.title+'</h2>'
           +'<p>'+articleData.description+'</p>'
@@ -34,3 +34,33 @@ xhr.onreadystatechange = function() {
   }
 }
 
+// Smooth scroll to second section
+var links = document.getElementsByClassName('scroll-link');
+
+for (var i = 0; i < links.length; i++) {
+    links[i].onclick = scroll;
+}
+
+function scroll(e) {
+    e.preventDefault();
+    var id = this.getAttribute('href').replace('#', '');
+    var target = document.getElementById(id).getBoundingClientRect().top;
+    animateScroll(target);
+}
+
+function animateScroll(targetHeight) {
+    targetHeight = document.body.scrollHeight - window.innerHeight > targetHeight + scrollY ? 
+        targetHeight : document.body.scrollHeight - window.innerHeight;
+    var initialPosition = window.scrollY;
+    var SCROLL_DURATION = 30;
+    var step_x = Math.PI / SCROLL_DURATION;
+    var step_count = 0;
+    requestAnimationFrame(step);
+    function step() {
+        if (step_count < SCROLL_DURATION) {
+            requestAnimationFrame(step);
+            step_count++;
+            window.scrollTo(0, initialPosition + targetHeight * 0.25 * Math.pow((1 - Math.cos(step_x * step_count)), 2));
+        }
+    }
+}
